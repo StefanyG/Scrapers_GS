779adb618a956b7b44904c2bbda8d09637f5ddcd
StefanyG <49376967+StefanyG@users.noreply.github.com>
2021-06-08 15:23:40 -0400

    cambios en los for y en el id_u
    para que la tabla de citas se guarde correctamente y las id de las instituciones solo guarde la oficial

--- AutorPaper.py
+++ AutorPaper.py
@@ -1,4 +1,4 @@
-import re, sqlite3, requests, time, urllib.parse, os
+import re, sqlite3, requests, time, urllib.parse, os, lxml
 from bs4 import BeautifulSoup
 
 conn = sqlite3.connect('scholar.db')
@@ -25,30 +25,37 @@ for url in urls:
         api_url = 'https://scholar.google.com/citations?hl=en&user={user}&cstart={start}&pagesize={pagesize}'
         user_id = re.search(r'user=(.*)', url).group(1)
         start = 0
-        time.sleep(2)
+        time.sleep(1)
     except:
         pass
     while True:
         soup = BeautifulSoup(requests.post(api_url.format(user=user_id, start=start, pagesize=1000)).content, 
-        'html.parser')
+        'lxml')
         
         name = soup.find('div', attrs={'id': 'gsc_prf_in'}).text
         name_attributes = soup.find_all('div', attrs={'class': 'gsc_prf_il'})
         affiliation = name_attributes[0].text or ''
         interests = name_attributes[2].text or ''
-        link = soup.find('a', attrs={'class': 'gsc_prf_ila'})
-        l = link.get('href')
-        id_u = l.split('=')[-1] or ''
         article = soup.find('tr', {'class': 'gsc_a_tr'})
         research_article = soup.find_all('tr',{'class':'gsc_a_tr'})
-
-        email = name_attributes[1].text
-        try:
-            verified, mail = email.split('Verified email at ')
-            dom, cl = mail.split('.')
+        try:   
+            elem = soup.find('a', href=re.compile('view_op=view_org&hl=en&oe=ASCII&org='))
+            idu= (elem['href'])    
+            id_u = idu.split('=')[-1] or ''
         except:
-            d, c= email.split('Verified email at ')
-            unod, dom, tresd =c.split('.')
+            id_u = ''
+            pass
+        try:
+            email = name_attributes[1].text
+            try:    
+                verified, mail = email.split('Verified email at ')
+                dom, cl = mail.split('.')
+            except:
+                d, c= email.split('Verified email at ')
+                unod, dom, tresd =c.split('.')
+        except:
+            dom = ''
+            pass
                      
         for i, article_info in enumerate(research_article, 1):
             try:
@@ -75,10 +82,10 @@ for url in urls:
                 cur.execute("""INSERT OR IGNORE INTO Autor (Id_AutorGS, Nombre_Autor, Cargo, Dominio, 
                     Intereses, Link, Id_InstitucionGS) VALUES (?, ?, ?, ?, ?, ?,?)""", tablaautor)
                 cur.execute("""INSERT OR IGNORE INTO Instituciones (Id_InstitucionGS) VALUES (?)""", [id_u])
-               # print(f'Data inserted: {id_a}')
+                print(f'Data inserted: {id_a}')
                 #id = cur.lastrowid
                 cur.execute("""INSERT OR IGNORE INTO Autor_Paper (Id_AutorGS, Titulo_Paper, Autores_Paper, Revista, 
-                    Citado_Por, Año, Link_Paper) VALUES ( ?, ?, ?, ?, ?, ?, ?)""",[ id_a, title, authors, 
+                    Citado_Por, Año, Link_Paper) VALUES ( ?, ?, ?, ?, ?, ?, ?)""",[id_a, title, authors, 
                     journal, cited_by, year, linkpaper])
                 conn.commit()
             except:
@@ -86,8 +93,29 @@ for url in urls:
         if len(research_article) != 100:
             break  
         start += 100        
-        try:        
-            for cited_by_public_access in soup.select('.gsc_rsb'):
+    try:  
+        for coautho in soup.find('ul', class_='gsc_rsb_a'):
+            nameco = coautho.find('a', attrs={'tabindex': '-1'})
+            coautor = nameco.text or ''
+            l = nameco.get('href')
+            c = l.split('=')[1]
+            idco = c.split('&')[-2] or ''
+            inst = coautho.find('span', attrs={'class': 'gsc_rsb_a_ext'}).text or ''
+            try:
+                dominioc = coautho.find(class_='gsc_rsb_a_ext gsc_rsb_a_ext2').get_text()
+                verc, maic = dominioc.split('Verified email at ')
+                doc, cc = maic.split('.')
+            except:
+                pass
+            tablacoautor=([idco, coautor, inst, doc, id_a])
+            cur.execute("""INSERT INTO Coautores (Id_Coautor, Nombre_Coautor, Cargo, Dominio,
+                    Id_AutorGS) VALUES (?, ?, ?, ?, ?)""", tablacoautor)
+            conn.commit()
+    except:
+        pass
+    try:
+           
+        for cited_by_public_access in soup.select('.gsc_rsb'):
                 year_since_text = cited_by_public_access.find_all('tr')[0]
                 citations = cited_by_public_access.find_all('tr')[1]
                 h_index = cited_by_public_access.find_all('tr')[2]
@@ -101,7 +129,7 @@ for url in urls:
                 i10_index_since = i10_index.find_all('td')[2].text or 0
                 tablaautorcitas = ([h_index_all, h_index_since, citation_all, citation_since, i10_index_since, 
                 i10_index_all])
-               # print(h_index_all)
+                #print(tablaautorcitas)
 
                 for row in cur.execute('SELECT * FROM Autor'):
                     cur.execute('''UPDATE OR IGNORE Autor SET Hindex_Todo=?, Hindex_Desde=?, Citas_Todo=?, 
@@ -109,42 +137,17 @@ for url in urls:
                     (h_index_all, h_index_since, citation_all, citation_since, i10_index_since, 
                     i10_index_all, id_a))
                     conn.commit()
-
-        except:
-            pass
-        try:
-                yearshist = soup.findAll('span', class_='gsc_g_t')
-                citashist = soup.findAll('span', class_='gsc_g_al')
-                for yearh, citah in zip(yearshist, citashist):
-                    yh = yearh.text or ''
-                    ch = citah.text or ''
+    except:
+        pass
+    try:
+        yearshist = soup.findAll('span', class_='gsc_g_t')
+        citashist = soup.findAll('span', class_='gsc_g_al')
+        for yearh, citah in zip(yearshist, citashist):
+            yh = yearh.text or ''
+            ch = citah.text or ''
                    # print(yh)
-                    cur.execute("""INSERT OR IGNORE INTO Citas (Id_AutorGS, Año_Historico, Citas_Historicas)
+            cur.execute("""INSERT OR IGNORE INTO Citas (Id_AutorGS, Año_Historico, Citas_Historicas)
                     VALUES ( ?, ?, ?)""",[id_a, yh, ch])
-                    conn.commit()   
-        except:
-            pass
-        try:  
-            for coautho in soup.find('ul', class_='gsc_rsb_a'):
-                nameco = coautho.find('a', attrs={'tabindex': '-1'})
-                coautor = nameco.text or ''
-                l = nameco.get('href')
-                c = l.split('=')[1]
-                idco = c.split('&')[-2] or ''
-                inst = coautho.find('span', attrs={'class': 'gsc_rsb_a_ext'}).text or ''
-                
-                try:
-                    dominioc = coautho.find(class_='gsc_rsb_a_ext gsc_rsb_a_ext2').get_text()
-            
-                    verc, maic = dominioc.split('Verified email at ')
-                    doc, cc = maic.split('.')
-                    print(dominioc)
-                except:
-                    pass
-                tablacoautor=([idco, coautor, inst, doc, id_a])
-
-                cur.execute("""INSERT INTO Coautores (Id_Coautor, Nombre_Coautor, Cargo, Dominio,
-                    Id_AutorGS) VALUES (?, ?, ?, ?, ?)""", tablacoautor)
-                conn.commit()
-        except:
-            pass
+            conn.commit()   
+    except:
+        pass
